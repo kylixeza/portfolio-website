27e6ed35a33606f00011a17f96b633d7873db096
Kylix Eza S <ksaputra@dopalearn.com>
2025-02-08 12:04:53 +0700

    added 404 redirect

--- src/main/kotlin/com/kylix/Routing.kt
+++ src/main/kotlin/com/kylix/Routing.kt
@@ -1,14 +1,22 @@
 package com.kylix
 
+import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.http.content.*
+import io.ktor.server.plugins.statuspages.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
 fun Application.configureRouting() {
+
+    install(StatusPages) {
+        status(HttpStatusCode.NotFound) { call, status ->
+            call.respondRedirect("/not-found")
+        }
+    }
+
     routing {
         staticResources("/", "resume")
-        // Static plugin. Try to access `/static/index.html`
-        //staticResources("/static", "static")
+        staticResources("/not-found", "resume", index = "404.html")
     }
 }
